81ed4c8fb436d9c7c5a8ee746f1777424b09c3ad
peer35 <p.j.m.vos@vu.nl>
2020-08-12 09:37:19 +0200

    refactor to GET

--- resources/getGeoJSONYear.php
+++ resources/getGeoJSONYear.php
@@ -5,18 +5,17 @@ header("Access-Control-Allow-Headers: *");
 include 'config.php';
 include('db.php');
 include('rowsToGeoJson.php');
-$pdata = file_get_contents('php://input');
-$post = json_decode($pdata, true);
-$begin = $post['begin'];
-$end = $post['end'];
+$year = filter_input(INPUT_GET, 'year', FILTER_VALIDATE_REGEXP, array(
+    "options" => array("regexp" => "/^\d{3,4}$/")
+));
 
 $db = new db();
 
-$cache_file = CACHEPATH . $begin . '.json';
+$cache_file = CACHEPATH . $year . '.json';
 if (file_exists($cache_file) && CACHE_ENABLED) {
     $json=file_get_contents($cache_file);
 } else {
-    $list = $db->getByPeriod($begin, $end);
+    $list = $db->getByPeriod($year, $year);
     $json = json_encode(createMinimalGeoJSON($list,'klooster'));
     file_put_contents($cache_file, $json);
 }
