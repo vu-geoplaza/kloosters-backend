535aee950d5b1f061b10f3a4e14ca6f8e133ce10
peer35 <p.j.m.vos@vu.nl>
2020-08-18 16:02:05 +0200

    config template

---
+++ resources/config.template.php
@@ -0,0 +1,22 @@
+<?php
+// do not commit
+//Define('DBHOST','fgw-www-dbs-srvr.vu.nl');
+Define('DBHOST','kloosterlijst-dbs-srvr.vu.nl');
+Define('DB','kloosterlijst-dbs');
+Define('DBNAME','******');
+Define('DBPW','******');
+
+
+Define('BRON1', 'Goudriaan, K. (2017). Kloosterlijst en Kloosterkaart ');
+Define('BRON2', ' [Data set]. Retrieved ');
+Define('BRON3', ', from http://geoplaza.vu.nl/projects/kloosters');
+Define('LICENSE', '<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>');
+
+Define('LIJSTIDURL','http://www2.fgw.vu.nl/oz/kloosterlijst/kdetails.php?ID=');
+Define('LIJSTFOTOURL','http://www2.fgw.vu.nl/oz/kloosterlijst/foto/');
+
+Define('KAARTIDURL','http://geoplaza.vu.nl/projects/kloosters/index.html?id=');
+Define('SYMBOLURL','http://geoplaza.vu.nl/projects/kloosters/svg/');
+
+define('CACHEPATH', '/content/map_cache/kloosters/');
+define('CACHE_ENABLED', true);
